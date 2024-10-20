c6242dc103e5c73c82b0043124509e7d5cf4d057
klpatel0024 <klpatel0024@gmail.com>
2024-10-19 21:10:07 -0400

    Add files via upload

---
+++ Resume.java
@@ -0,0 +1,14 @@
+public class Resume
+{
+    private String name;
+    private String address;
+    private String phone;
+    private String email;
+    private String objective;
+    private String education;
+    private String workExperience;
+    private String skills;
+    private String certifications;
+    private String volunteering;
+    
+}
