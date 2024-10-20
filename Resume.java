f995ffe39cc6c788a1ec8d71c27757d468f2759f
joa328 <joa328@lehigh.edu>
2024-10-19 21:39:22 -0400

    Actual upload of accessor and mutator methods
    Signed-off-by: joa328 <joa328@lehigh.edu>

--- Resume.java
+++ Resume.java
@@ -11,4 +11,36 @@ public class Resume
     private String certifications;
     private String volunteering;
     
+
+
+    public String getName(){return name;}
+    public void setName(String n){name = n;}
+
+    public String getAddress(){return address;}
+    public void setAddress(String a){address = a;}
+
+    public String getPhone(){return phone;}
+    public void setPhone(String p){phone = p;}
+
+    public String getEmail(){return email;}
+    public void setEmail(String e){email = e;}
+
+    public String getObjective(){return objective;}
+    public void setObjective(String o){objective = o;}
+
+    public String getEducation(){return education;}
+    public String setEducation(String e){education = e;}
+
+    public String getWorkExperience(){return workExperience;}
+    public void setWorkExperience(String w){workExperience = w;}
+
+    public String getSkills(){return skills;}
+    public void setSkills(String s){skills = s;}
+
+    public String getCertifications(){return certifications;}
+    public void setCertification(String c){certifications = c;}
+
+    public String getVolunteering(){return volunteering;}
+    public void setVolunteering(String v){volunteering = v;}
+
 }
